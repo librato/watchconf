56cb357ad9338458e6d61ecbe45852f95cc2adfb
Ray Jenkins <rjenkins@aceevo.com>
2015-01-05 17:44:10 -0600

    adding javadoc to interfaces

--- src/main/java/com/librato/watchconf/converter/Converter.java
+++ src/main/java/com/librato/watchconf/converter/Converter.java
@@ -1,8 +1,28 @@
 package com.librato.watchconf.converter;
 
+
+/**
+ * A base interface for serializing and de-serializing objects from to and front a byte[]
+ * @param <T> The type of object this converter converts.
+ */
 public interface Converter<T> {
 
+    /**
+     * Converts a byte[] to an instance of <T>
+     *
+     * @param bytes The serialized object in bytes.
+     * @param clazz The class of type T
+     * @return a deserialized instance of <T>
+     * @throws Exception
+     */
     T toDomain(byte[] bytes, Class<T> clazz) throws Exception;
 
+    /**
+     * Converts an instance of <T> to a byte[]
+     *
+     * @param t and instance of type <T>
+     * @return the serialized byte[] of t
+     * @throws Exception
+     */
     byte[] fromDomain(T t) throws Exception;
 }
--- src/main/java/com/librato/watchconf/converter/JsonConverter.java
+++ src/main/java/com/librato/watchconf/converter/JsonConverter.java
@@ -5,15 +5,33 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
+/**
+ * An implementation of {@link com.librato.watchconf.converter.Converter} that serializes and de-serializes
+ * JSON/POJOs
+ * @param <T>
+ */
 public class JsonConverter<T> implements Converter<T> {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
+    /**
+     * Converts an byte[] of JSON to an instance of the type referenced by this class.
+     * @param bytes serialized json in bytes
+     * @param clazz the class of the type referenced by this class.
+     * @return an instance of type <T>
+     * @throws IOException
+     */
     @Override
     public T toDomain(byte[] bytes, Class<T> clazz) throws IOException {
         return objectMapper.readValue(bytes, clazz);
     }
 
+    /**
+     * Converts and instance of type <T> to JSON serialized byte[]
+     * @param t an instance of type <T>
+     * @return bytes of serialized JSON for t
+     * @throws JsonProcessingException
+     */
     @Override
     public byte[] fromDomain(T t) throws JsonProcessingException {
         return objectMapper.writeValueAsBytes(t);
