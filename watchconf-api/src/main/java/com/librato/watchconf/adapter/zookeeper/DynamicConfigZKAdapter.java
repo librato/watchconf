6b6805d9971c01bd38cb3a60e21d2d430e27b140
Ray Jenkins <rjenkins@aceevo.com>
2015-01-12 16:55:19 -0600

    handle race in cluster on create zk node if absent

--- watchconf-api/src/main/java/com/librato/watchconf/adapter/zookeeper/DynamicConfigZKAdapter.java
+++ watchconf-api/src/main/java/com/librato/watchconf/adapter/zookeeper/DynamicConfigZKAdapter.java
@@ -2,24 +2,35 @@ package com.librato.watchconf.adapter.zookeeper;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
+import com.librato.watchconf.DynamicConfig;
 import com.librato.watchconf.adapter.AbstractConfigAdapter;
 import com.librato.watchconf.converter.Converter;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.imps.CuratorFrameworkState;
 import org.apache.curator.framework.recipes.cache.NodeCache;
 import org.apache.curator.framework.recipes.cache.NodeCacheListener;
+import org.apache.zookeeper.KeeperException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class DynamicConfigZKAdapter<T> extends AbstractConfigAdapter<T, byte[]> {
 
+    private Logger log = LoggerFactory.getLogger(DynamicConfig.class);
     private NodeCacheListener nodeCacheListener;
     private NodeCache nodeCache;
 
     public DynamicConfigZKAdapter(final String path, final CuratorFramework curatorFramework, Converter<T, byte[]> converter, ChangeListener<T> changeListener) throws Exception {
         super(converter, Optional.fromNullable(changeListener));
-        Preconditions.checkArgument(path != null && !path.isEmpty(), "path cannot be null or blank");
         Preconditions.checkNotNull(curatorFramework, "CuratorFramework cannot be null");
+        Preconditions.checkArgument(curatorFramework.getState() == CuratorFrameworkState.STARTED, "CuratorFramework must be started");
+        Preconditions.checkArgument(path != null && !path.isEmpty(), "path cannot be null or blank");
 
         if (curatorFramework.checkExists().forPath(path) == null) {
-            curatorFramework.create().creatingParentsIfNeeded().forPath(path);
+            try {
+                curatorFramework.create().creatingParentsIfNeeded().forPath(path);
+            } catch (KeeperException.NodeExistsException ex) {
+                log.info("Node exists on create, continuing");
+            }
         }
 
         getAndSet(curatorFramework.getData().forPath(path));
