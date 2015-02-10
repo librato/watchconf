7b9d388dcdfa2003f73d175e3a6a740bfba5e562
Ray Jenkins <rjenkins@aceevo.com>
2015-02-09 19:06:01 -0600

    adding class validation support for watchconf-util

--- watchconf-util/src/main/java/com/librato/watchconf/util/WatchConf.java
+++ watchconf-util/src/main/java/com/librato/watchconf/util/WatchConf.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.common.collect.ImmutableMap;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 public class WatchConf {
 
@@ -23,22 +25,25 @@ public class WatchConf {
     }
 
     private static void pushConfig(String[] args) throws Exception {
-        String zkConnectFlag = args[0];
-        String formatFlag = args[2];
-        String fileFlag = args[4];
-        String outputFlag = args[6];
-        String znodeFlag = args[8];
+        ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
+        for (int i = 0; i < args.length; i++) {
+            if (i % 2 == 0 && i + 1 < args.length) {
+                builder.put(args[i], args[i + 1]);
+            }
+        }
 
-        if (!"-zkServer".equals(zkConnectFlag) || !"-format".equals(formatFlag) || !"-f".equals(fileFlag) || !"-o".equals(outputFlag) || !"-z".equals(znodeFlag)) {
+        Map<String, String> argMap = builder.build();
+
+        String zkServer = argMap.get("-zkServer");
+        String format = argMap.get("-format");
+        String fileName = argMap.get("-f");
+        String output = argMap.get("-o");
+        String path = argMap.get("-z");
+        if (zkServer == null || format == null || fileName == null || output == null || path == null) {
             printHelp();
             return;
         }
 
-        String zkServer = args[1];
-        String format = args[3];
-        String fileName = args[5];
-        String output = args[7];
-        String path = args[9];
 
         JsonNode jsonNode = getJsonNode(new File(fileName), format);
 
@@ -50,8 +55,22 @@ public class WatchConf {
         framework.start();
 
         byte[] outputBytes = getOutputBytes(output, jsonNode);
+
+        String className = argMap.get("-c");
+        if (className != null) {
+            Class clazz = Class.forName(className);
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                objectMapper.readValue(outputBytes, clazz);
+            } catch (Exception ex) {
+                ex.printStackTrace(System.err);
+                System.out.println("error converting data to class: " + className + " please fix input error before continuing");
+                return;
+            }
+        }
+
         try {
-            if(framework.checkExists().forPath(path) == null) {
+            if (framework.checkExists().forPath(path) == null) {
                 framework.create().creatingParentsIfNeeded().forPath(path, outputBytes);
             } else {
                 framework.setData().forPath(path, outputBytes);
@@ -95,6 +114,7 @@ public class WatchConf {
         System.out.println("-format [yaml|json]: input file format");
         System.out.println("-f <file>: input file to read from");
         System.out.println("-o [yaml|json]: format of data to output to znode");
-        System.out.println("-z: full path to znode to update, will create parents and node doesn't exist");
+        System.out.println("-z full path to znode to update, will create parents and node doesn't exist");
+        System.out.println("-c (Optional) name of class to validate JSON against before pushing");
     }
 }
